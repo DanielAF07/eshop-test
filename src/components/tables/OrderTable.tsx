3a61055be1749de55503f8f5c3ad013ffe0d0d59
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-03-09 16:42:49 -0600

    Add payment functionality to OrderTable component

--- src/components/tables/OrderTable.tsx
+++ src/components/tables/OrderTable.tsx
@@ -7,20 +7,30 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table'
+import formatNumber from '@/helpers/formatNumber'
 import { Order } from '@/interfaces/Order'
 import { useCallback } from 'react'
+import { Button } from '../ui/button'
+import { toast } from 'sonner'
+import { useRouter } from 'next/router'
 
 interface Props {
   order: Order | null
 }
 
 export function OrderTable ({ order }: Props) {
+  const router = useRouter()
   // Callback to calculate the total of the order
   const getTotal = useCallback(() => {
     return order?.items.reduce((acc, item) => {
       return acc + Number(item.quantity) * Number(item.price)
     }, 0)
   }, [order])
+
+  const handlePayment = () => {
+    toast.success('Payment successful')
+    router.replace('/orders')
+  }
 
   if (!order) return <div>Loading...</div>
 
@@ -38,19 +48,27 @@ export function OrderTable ({ order }: Props) {
       </TableHeader>
       <TableBody>
         {order.items.map((item) => (
-          <TableRow key={item.id}>
+          <TableRow key={item.sku}>
             <TableCell className='font-medium'>{item.sku ?? '-'}</TableCell>
             <TableCell>{item.name}</TableCell>
             <TableCell>{item.quantity}</TableCell>
-            <TableCell>{item.price}</TableCell>
-            <TableCell className='text-right'>{Number(item.quantity) * Number(item.price)}</TableCell>
+            <TableCell>${formatNumber(item.price)}</TableCell>
+            <TableCell className='text-right'>${formatNumber(Number(item.quantity) * Number(item.price))}</TableCell>
           </TableRow>
         ))}
       </TableBody>
       <TableFooter>
         <TableRow>
           <TableCell colSpan={4}>Total</TableCell>
-          <TableCell className='text-right'>{getTotal()}</TableCell>
+          <TableCell className='text-right text-lg'>$ {formatNumber(getTotal() as number)}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell colSpan={4} />
+          <TableCell className='text-right text-lg'>
+            <Button onClick={handlePayment}>
+              Pay
+            </Button>
+          </TableCell>
         </TableRow>
       </TableFooter>
     </Table>
