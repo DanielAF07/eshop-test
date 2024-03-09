701f9cc475be3782f04e13c41e3074f0817de40c
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-03-09 16:54:35 -0600

    Remove unused imports and query client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import {
-  QueryClient,
-  QueryClientProvider
-} from '@tanstack/react-query'
 import { Toaster } from 'sonner'
-
-const queryClient = new QueryClient()
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Toaster richColors position='top-center' />
       <Component {...pageProps} />
-    </QueryClientProvider>
+    </>
   )
 }
