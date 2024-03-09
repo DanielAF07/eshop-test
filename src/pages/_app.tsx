0c113a1d895f9272bc3ab85fd37dae8444909b1b
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2024-03-09 16:23:28 -0600

    Add Toaster component to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,14 @@ import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 
 const queryClient = new QueryClient()
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster richColors position='top-center' />
       <Component {...pageProps} />
     </QueryClientProvider>
   )
